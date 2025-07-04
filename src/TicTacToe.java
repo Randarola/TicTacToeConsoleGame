79320fc85342b1d84dc6266c34d6a4597304262e
Randarola <140419343+Randarola@users.noreply.github.com>
2025-07-04 23:24:24 +0200

    add initializeBoard

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,8 +2,18 @@ import java.util.Random;
 import java.util.Scanner;
 public class TicTacToe {
     private static char[][] board = new char[3][3];
+    private static char currentPlayer = 'X';
+    private static Random random = new Random();
     public static void main(String[] args) {
 
 
     }
+    private static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
 }
