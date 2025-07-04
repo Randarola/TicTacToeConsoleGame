613d4bec8aedd9a3dc773de3c2c7f69161c29e0e
Randarola <140419343+Randarola@users.noreply.github.com>
2025-07-04 23:25:51 +0200

    add isBoardFull

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -27,5 +27,13 @@ public class TicTacToe {
             if (i < 2) System.out.println("  -----");
         }
     }
+    private static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') return false;
+            }
+        }
+        return true;
+    }
 
 }
